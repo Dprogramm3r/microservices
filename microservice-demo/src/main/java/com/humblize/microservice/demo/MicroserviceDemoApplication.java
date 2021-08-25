8577b697a3f69acd62613165e7a2bb8b8a8b4195
joshua <silverjosh@hotmail.co.uk>
2021-08-25 12:39:26 +0100

    first commit

---
+++ microservice-demo/src/main/java/com/humblize/microservice/demo/MicroserviceDemoApplication.java
@@ -0,0 +1,14 @@
+package com.humblize.microservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDemoApplication.class, args);
+
+	}
+
+}
